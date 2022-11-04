c3287b499065638805f273bc8874cfbf430591db
jake-manning <jcmanning92@gmail.com>
2022-11-03 23:13:02 -0700

    ETL queries for loading in .csv files

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,39 +1,50 @@
 \c qa
 
-DROP TABLE IF EXISTS questions;
-DROP TABLE IF EXISTS answers;
-DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS answers CASCADE;
+DROP TABLE IF EXISTS answers_photos CASCADE;
 
-CREATE TABLE 
-  questions (
-    id INT(11) AUTO_INCREMENT,
-    product_id INT(11) NOT NULL,
-    body TEXT(1000) NOT NULL,
-    date_written DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    asker_name TEXT(60) NOT NULL,
-    asker_email TEXT(60) NOT NULL,
-    reported BOOLEAN NOT NULL DEFAULT 0,
-    helpfulness INT(11) NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
+CREATE TABLE questions (
+    "id" INT NOT NULL PRIMARY KEY,
+    "product_id" INT NOT NULL,
+    "body" VARCHAR(1000) NOT NULL,
+    "date_written" BIGINT NOT NULL,
+    "asker_name" VARCHAR(60) NOT NULL,
+    "asker_email" VARCHAR(60) NOT NULL,
+    "reported" BOOLEAN NOT NULL DEFAULT false,
+    "helpfulness" INT NOT NULL DEFAULT 0
   );
 
-CREATE TABLE 
-  answers (
-    id INT(11) AUTO_INCREMENT,
-    question_id INT(11) NOT NULL REFERENCES questions(id),
-    body TEXT(60) NOT NULL,
-    date_written DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    answerer_name TEXT(60) NOT NULL,
-    answerer_email TEXT(60) NOT NULL,
-    reported BOOLEAN NOT NULL DEFAULT 0,
-    helpfulness INT(11) NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
+COPY questions
+FROM '/Users/jake-manning/SDC_Data/questions.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE answers (
+    "id" INT NOT NULL PRIMARY KEY,
+    "question_id" INT NOT NULL,
+    "body" VARCHAR(1000) NOT NULL,
+    "date_written" BIGINT NOT NULL,
+    "answerer_name" VARCHAR(60) NOT NULL,
+    "answerer_email" VARCHAR(60) NOT NULL,
+    "reported" BOOLEAN NOT NULL DEFAULT '0',
+    "helpfulness" INT NOT NULL DEFAULT 0,
+    FOREIGN KEY ("question_id") REFERENCES questions("id")
   );
 
-CREATE TABLE 
-  answers_photos (
-    id INT(11) AUTO_INCREMENT,
-    url TEXT(500) NOT NULL,
-    answer_id INT(11) NOT NULL REFERENCES answers(id),
-    PRIMARY KEY (id)
+COPY answers
+FROM '/Users/jake-manning/SDC_Data/answers.csv'
+DELIMITER ','
+CSV HEADER;
+
+CREATE TABLE answers_photos (
+    "id" INT NOT NULL PRIMARY KEY,
+    "answer_id" INT NOT NULL,
+    "url" TEXT NOT NULL,
+    FOREIGN KEY ("answer_id") REFERENCES answers("id")
 );
+
+COPY answers_photos
+FROM '/Users/jake-manning/SDC_Data/answers_photos.csv'
+DELIMITER ','
+CSV HEADER;
